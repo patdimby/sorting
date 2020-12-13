200b532da2147ded4c3474064e072e0ede5c02bc
RANOELISON Dimbisoa Patrick <dimbisoapatrick@gmail.com>
2020-12-13 11:33:27 +0300

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -2,6 +2,50 @@
 That is, a sorting algorithm is stable if whenever there are two records R and S with the same key 
 and with R appearing before S in the original list, R will appear before S in the sorted list. '''
 
+# quick sort is not stable sort
+def quickSort(A, low, high):
+    '''Quick short.
+    Worst-case performance:
+        O(n2)
+    Best case performance:
+        O(n log n) (simple partition)
+        or O(n) (three-way partition and equal keys)
+    Average performance	O(n log n)
+   
+    '''
+    if low < high:
+        pivot = lomuto_partition(A, low, high)
+        quickSort(A, low, pivot - 1)
+        quickSort(A, pivot + 1, high)
+
+def lomuto_partition(A, low, high):
+    '''Worst-case space complexity:
+    O(n) auxiliary (naive)'''
+    pivot = A[high]
+    i = low
+    for j in range(low, high):
+        if A[j] < pivot:
+            A[i], A[j] = A[j], A[i]
+            i = i + 1
+    A[i], A[high] = A[high], A[i]
+    return i
+
+def hoare_partition(A, low, high):
+    ''' Worst-case space complexity:
+        O(log n) auxiliary (Hoare 1962)
+    '''
+    pivot = A[int((high + low) / 2)]
+    i = low - 1
+    j = high + 1
+    if j == len(A):
+        j = j - 1
+    while A[i] < pivot:
+        i = i + 1
+    while A[j] > pivot: 
+        j = j - 1
+    if i >= j:
+        return j
+    A[i], A[j] = A[j], A[i]
 
 def countSort(input, k):
     '''Linear short member.
